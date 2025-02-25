f24d1788cc6561b96db981056e80ba1def3c77ee
jangrarajat <rajatjangra940@gmail.com>
2025-02-26 01:04:42 +0530

    Javascript notes

---
+++ script.js
@@ -0,0 +1,68 @@
+    console.log("welcome back hellow rajat");
+    console.log("hellow rajat i am a programer");
+
+//  <!-- Variables in js -->
+      {   fullName="Rajat jangra";
+           age = 21;
+           a = null;
+           isfollow=true;
+        console.log(fullName);
+        console.log(age);
+        console.log(a);
+        console.log(isfollow);}
+
+ // <!-- Varieble Keywords -->
+      { let age = 21;
+        age = 24;
+        age = 27;
+        console.log(age)
+       const a = (12);
+        console.log(a);
+     const cast = "Jangra";
+        console.log(cast);} 
+// <!-- data types in JavaScripe -->
+// Primitive deta  : Number,String,Boolean,Undefined,Null,Bigint,Symbol.
+               {let fullName ="Rajat jangra";
+                console.log(fullName)
+               }
+   // Non-Primitive data(key:value)
+       { const student = {
+            fullname : "Rajat Jangra",
+            age : 21,
+            ispass : true
+    
+        };
+        student["age"] = student ["age"] + 2
+        console.log(student);
+        console.log(student["age"])
+    }
+
+
+
+
+
+
+                
+                const product = {
+                    name:"Ball Pen",
+                    rating:4.5,
+                    prise:10,
+                    offer:5
+                }
+                console.log(product);
+               console.log(product["prise"]);
+                
+
+
+
+
+
+
+
+
+
+        
+
+       
+
+   
