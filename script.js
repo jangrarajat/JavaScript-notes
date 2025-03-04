756478682bdafa8a227866e18b3fb989289eb60d
jangrarajat <rajatjangra940@gmail.com>
2025-03-04 17:21:22 +0530

    Adding Operators chaptors

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-    console.log("welcome back hellow rajat");
-    console.log("hellow rajat i am a programer");
+//     console.log("welcome back hellow rajat");
+//     console.log("hellow rajat i am a programer");
 
-//  <!-- Variables in js -->
-      {   fullName="Rajat jangra";
-           age = 21;
-           a = null;
-           isfollow=true;
-        console.log(fullName);
-        console.log(age);
-        console.log(a);
-        console.log(isfollow);}
+// //  <!-- Variables in js -->
+//       {   fullName="Rajat jangra";
+//            age = 21;
+//            a = null;
+//            isfollow=true;
+//         console.log(fullName);
+//         console.log(age);
+//         console.log(a);
+//         console.log(isfollow);}
 
- // <!-- Varieble Keywords -->
-      { let age = 21;
-        age = 24;
-        age = 27;
-        console.log(age)
-       const a = (12);
-        console.log(a);
-     const cast = "Jangra";
-        console.log(cast);} 
-// <!-- data types in JavaScripe -->
-// Primitive deta  : Number,String,Boolean,Undefined,Null,Bigint,Symbol.
-               {let fullName ="Rajat jangra";
-                console.log(fullName)
-               }
-   // Non-Primitive data(key:value)
-       { const student = {
-            fullname : "Rajat Jangra",
-            age : 21,
-            ispass : true
+//  // <!-- Varieble Keywords -->
+//       { let age = 21;
+//         age = 24;
+//         age = 27;
+//         console.log(age)
+//        const a = (12);
+//         console.log(a);
+//      const cast = "Jangra";
+//         console.log(cast);} 
+// // <!-- data types in JavaScripe -->
+// // Primitive deta  : Number,String,Boolean,Undefined,Null,Bigint,Symbol.
+//                {let fullName ="Rajat jangra";
+//                 console.log(fullName)
+//                }
+//    // Non-Primitive data(key:value)
+//        { const student = {
+//             fullname : "Rajat Jangra",
+//             age : 21,
+//             ispass : true
     
-        };
-        student["age"] = student ["age"] + 2
-        console.log(student);
-        console.log(student["age"])
-    }
+//         };
+//         student["age"] = student ["age"] + 2
+//         console.log(student);
+//         console.log(student["age"])
+//     }
 
 
 
@@ -43,26 +43,70 @@
 
 
                 
-                const product = {
-                    name:"Ball Pen",
-                    rating:4.5,
-                    prise:10,
-                    offer:5
-                }
-                console.log(product);
-               console.log(product["prise"]);
+//                 const product = {
+//                     name:"Ball Pen",
+//                     rating:4.5,
+//                     prise:10,
+//                     offer:5
+//                 }
+//                 console.log(product);
+//                console.log(product["prise"]);
                 
 
 
+//               /* Arithmetic Operators*/
+              
+// for (let num = 0; num <= 100 ;num++ ){
+//     if (num%2 !== 0) {
+//         console.log("even num =", num);
+//     } else{
+
+//     }
+// }
+
+// let password = "Rajat jangra";
+// let userpassword  = prompt("please enter you password :");
+
+//  let gameNum = "Rajat jangra";
+//  let userNum = prompt("Please Enter your Password  : ");
+
+//  while ( userNum != gameNum) {
+//     userNum = prompt( "You enttered wrong Password. please traying again : ");
+
+//  }
+//     console.log("Password is Currect");    
 
 
+//    let a = 2
+//    let b = 5
 
+//    console.log(a+b,a*b,a)
 
+                                        // let a = 5
+                                        // let b = 2
+                                        // let c = "5"
+        //    Conditionl Statments                               // console.log ( !(a===c))
+                                        
+   //   let age = prompt("Please Enter youre age")
 
+   //   if((age>=18)&&(age<=100)){
+   //      console.log("yes you can vote");
+   //   }
+   //   else if(age>=100){
+   //      console.log("you are expir for voting");
+   //   }
+   //   else{
+   //      console.log("you cannt vote");
+   //   }
+     
 
-
-        
 
        
 
-   
+   // _______________________________________
+   // |            Loops in JS                |
+   // |_______________________________________|
+   //      : Loops are used to execute a piece of code again & again
+
+      //   Example -->
+              
