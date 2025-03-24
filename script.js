0f06481c4c5626e4f2f5cea5b96e8a7bd9485bbd
jangrarajat <rajatjangra940@gmail.com>
2025-03-24 10:45:05 +0530

    adding a vowel counting app

--- script.js
+++ script.js
@@ -30,7 +30,7 @@
 //             fullname : "Rajat Jangra",
 //             age : 21,
 //             ispass : true
-    
+
 //         };
 //         student["age"] = student ["age"] + 2
 //         console.log(student);
@@ -42,7 +42,7 @@
 
 
 
-                
+
 //                 const product = {
 //                     name:"Ball Pen",
 //                     rating:4.5,
@@ -51,11 +51,11 @@
 //                 }
 //                 console.log(product);
 //                console.log(product["prise"]);
-                
+
 
 
 //               /* Arithmetic Operators*/
-              
+
 // for (let num = 0; num <= 100 ;num++ ){
 //     if (num%2 !== 0) {
 //         console.log("even num =", num);
@@ -82,51 +82,51 @@
 
 //    console.log(a+b,a*b,a)
 
-                                        // let a = 5
-                                        // let b = 2
-                                        // let c = "5"
-        //    Conditionl Statments                               // console.log ( !(a===c))
-                                        
-   //   let age = prompt("Please Enter youre age")
+// let a = 5
+// let b = 2
+// let c = "5"
+//    Conditionl Statments                               // console.log ( !(a===c))
 
-   //   if((age>=18)&&(age<=100)){
-   //      console.log("yes you can vote");
-   //   }
-   //   else if(age>=100){
-   //      console.log("you are expir for voting");
-   //   }
-   //   else{
-   //      console.log("you cannt vote");
-   //   }
-     
+//   let age = prompt("Please Enter youre age")
+
+//   if((age>=18)&&(age<=100)){
+//      console.log("yes you can vote");
+//   }
+//   else if(age>=100){
+//      console.log("you are expir for voting");
+//   }
+//   else{
+//      console.log("you cannt vote");
+//   }
 
 
-       
 
-   // _______________________________________
-   // |            Loops in JS                |
-   // |_______________________________________|
-   //      : Loops are used to execute a piece of code again & again
 
-      //   Example -->
-              
 
-   //  let hero = ["ironman","hulk","spiderman","flash"]
-   //             console.log( hero[3])
-             
-   //             for (let i = 0; i < hero.length ; i++){
-   //                console.log(hero[i])
-   //             }
+// _______________________________________
+// |            Loops in JS                |
+// |_______________________________________|
+//      : Loops are used to execute a piece of code again & again
+
+//   Example -->
+
+
+//  let hero = ["ironman","hulk","spiderman","flash"]
+//             console.log( hero[3])
+
+//             for (let i = 0; i < hero.length ; i++){
+//                console.log(hero[i])
+//             }
 
 // arro funcations
 
-   //  const my = (a, b) => {
-   //          return a + b
-   //  }
+//  const my = (a, b) => {
+//          return a + b
+//  }
 
 
-   //  const mymy = () => {
-   //    return"Your Password is worng ";
+//  const mymy = () => {
+//    return"Your Password is worng ";
 
 
 //   function vowels(str){
@@ -168,7 +168,7 @@
 
 // // This is for loop
 // for (let i = 1 ; i <= 10 ; i++){
-  
+
 //    console.log(i)
 // }
 // // break in for loop
@@ -195,7 +195,7 @@
 // console.log(str.substring(0 , 4))
 
 // let sent = "hellow \\rajat\\ kase \\ ho \\jldi\\ btaao";
- 
+
 // let words = sent.split('\\');
 
 // console.log(words);
@@ -216,3 +216,123 @@
 // }
 //  let ans = myfung(2,10)
 //  console.log(ans)
+// let fullName = prompt("Enter youer FullName")
+// let gmail = "@gmail.com"
+// console.log(fullName.concat(gmail))
+
+
+
+
+
+// let btn1 = document.querySelector("#btn1")
+// let dot = document.querySelector("#dot")
+// let mode = "light" ;
+
+//     btn1.onclick = () => {
+//       if(mode === "light"){
+//        mode = "dark"
+//        document.querySelector("body").style.backgroundColor = "black";
+//       }else  {
+//          mode = "light";
+//          document.querySelector("body").style.backgroundColor = "white" ;
+//       }
+
+
+
+//     }
+
+
+// function myfun(msg) {
+//    console.log(msg)
+//    console.log(msg)
+//    console.log(msg)
+// }
+
+
+//  myfun("Hello my lovee")
+
+
+
+
+// function num(mode){
+//    if ( mode === "dark"){
+//       mode = "light"
+//       document.querySelector("body").style.backgroundColor = "red";
+//       return mode
+//    } else{
+//       mode = "dark"
+//       document.querySelector("body").style.backgroundColor = "orange";
+//       return mode
+//    }
+// }
+
+//    // let newMd = num(prompt("enter mode"))
+
+//  let btn = document.querySelector("#btn1")
+//  btn.onclick = () => {
+//    num(prompt("enter mode"))
+//  }
+
+let btn = document.querySelector("#btn1")
+let output = document.querySelector("#output")
+let mode = "light"
+
+btn.onclick = () => {
+   if (mode === "light") {
+      mode = "dark"
+      document.querySelector("body").style.backgroundColor = "orange"
+      document.querySelector("#output").style.backgroundColor = "red"
+
+   } else {
+      mode = "light"
+      document.querySelector("body").style.backgroundColor = "red"
+      document.querySelector("#output").style.backgroundColor = "orange"
+
+   }
+}
+
+
+
+
+
+// let latter = prompt("ente youer latter")
+// let msg = document.querySelector("#msg")
+// function numchk() {
+//    let count = 0;
+//    for (const char of latter) {
+//       if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u")
+     
+//    count++}
+// msg.innerText = count
+// console.log(count)
+// }
+// numchk()
+
+let input = document.querySelector("#input")
+let submit = document.querySelector("#submit")
+
+
+submit.onclick = () =>{
+ let inputV = document.querySelector("#input ");
+let inputWord = inputV.value;
+ 
+let msg = document.querySelector("#msg")
+    function numchk() {
+       let count = 0;
+       for (const char of inputWord) {
+          if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u")      
+       count++}
+    msg.innerText = count
+    console.log(count)
+    }
+    numchk()
+}
+
+
+
+
+
+
+
+
+
