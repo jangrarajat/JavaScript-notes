28745408629436621f6e539294d589af973356c6
jangrarajat <rajatjangra940@gmail.com>
2025-03-27 18:02:49 +0530

    new update

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ let mode = "light"
 btn.onclick = () => {
    if (mode === "light") {
       mode = "dark"
-      document.querySelector("body").style.backgroundColor = "white"
-      document.querySelector("#output").style.backgroundColor = "rgb(168, 156, 179)"
-document.querySelector(".listdiv").style.backgroundColor = "rgba(0, 0, 0, 0.58)"
-
+      document.querySelector("body").style.backgroundColor = "rgb(58, 55, 60)";
+      document.querySelector("#output").style.backgroundColor = "rgba(70, 66, 75, 0.8)";
+   document.querySelector(".listdiv").style.backgroundColor = "rgba(0, 0, 0, 0.58)";
+document.querySelectorAll(".option").style.backgroundColor = "rgba(0, 0, 0, 0.87)";
+document.querySelector(".nav").style.backgroundColor = "white";
    } else {
       mode = "light"
-      document.querySelector("body").style.backgroundColor = "rgb(93, 30, 164)"
-      document.querySelector("#output").style.backgroundColor = "rgb(137, 69, 200)"
-document.querySelector(".listdiv").style.backgroundColor = "rgba(176, 182, 182, 0.837)"
-   }
+      document.querySelector("body").style.backgroundColor = "white"
+      document.querySelector("#output").style.backgroundColor = "rgba(209, 209, 209, 0.539)";
+document.querySelector(".listdiv").style.backgroundColor = "rgba(176, 182, 182, 0.837)";
+document.querySelectorAll(".option").style.backgroundColor = " #e8e8e8e1";
+  document.querySelector(".nav").style.backgroundColor = "rgb(58, 55, 60)";
+}
 }
 
 
@@ -57,49 +60,45 @@ let vowelOutput = document.querySelector("#output")
    if (listswich === "yesokbabu"){
       
       listswich = "nookbabu"
-      console.log("no")
+     
       document.querySelector(".listdiv").style.visibility = "visible";
       
    } else{
      
       listswich = "yesokbabu"
       document.querySelector(".listdiv").style.visibility = "hidden";
-      console.log("yes")
+    
        
    }
   }
+ let vowelChakerbtn = document.querySelector("#vowelChakerbtn")
+  let vowelChaker = "yesvowelChaker"
+
+  vowelChakerbtn.onclick = ( ) =>{                 
+                                       
+   if (vowelChaker === "yesvowelChaker"){
+      
+     
+     console.log("hi")
+      document.querySelector("#output").style.visibility = "visible";
+      vowelChaker = "novowelChaker"
+      document.querySelector(".listdiv").style.visibility = "hidden";
+   } else{
+      console.log("hellow")
+      vowelChaker = "yesvowelChaker"
+      document.querySelector("#output").style.visibility = "hidden";
+      document.querySelector(".listdiv").style.visibility = "hidden";
+       listswich = "yesvowelChaker"
+   }
+  }
  
+
+  let loginbtn = document.querySelector("#Login")
+  let desbord = document.querySelector("#desbord")
  
-// let rj = ["Name","Rajat","Age","rj","Addresh","Hamindpur"]
 
-// rj.forEach(function inter(value,index)
-// {
-//    console.log(value.toUpperCase(),index,value[0].toLocaleUpperCase())
-// }
-// )
-// let  numb = [1,2,3,4,5,6,7,8,9,10]
-// numb.forEach(function squearofnum(value)
-//   {
-//    console.log(value**2)
-//   }
-// )
-
-
-//  let list1 = ["aalu" , "ramater" ,"pyaaj"]
-//  let list2 = ["aaluxx" , "ramaterxx" ,"pyaajxx"]
-// let  str = "ja ng  ra"
+  loginbtn.onclick = () =>{
+   
+  
        
-
-//    let newliat = list1.concat(list2)
-//    console.log(newliat)
-
-//    newliat.forEach(function mufun(value,idx,arr){
-// console.log(value,idx,arr)
-//    }
-// )
-
-
-// let input1 = "120@gmail.com"
-// let input2 = prompt("type your input")
-
-// console.log(input2.concat(input1))0
+  }
